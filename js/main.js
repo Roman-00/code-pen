8fd04cc2bfe219f402cce247fbe08d84cf3cde9c
Roman00 <rvasin00@gmail.com>
2020-04-18 17:31:23 +0600

    fixed: code

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@
 
 function getResult(x,y){
   let result = x ** y;
-  
+
+  let num = String(result).split('').map(function(e){return parseInt(e)}).reduce(function(a,b){return a+b});
+
+  console.log(num);
+
   return result;
 }
 
-console.log(getResult(4, 8))
+console.log(getResult(4, 8));
