6a3e450f806d08dec0108c0cf2762299707aa062
Roman00 <rvasin00@gmail.com>
2020-04-18 19:18:14 +0600

    fead: code done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 'use strict';
 
-function getResult(x,y){
-  let result = x ** y;
+const myLesson = [
+  {lesson: 1, status: true},
+  {lesson: 2, status: true},
+  {lesson: 3, status: false},
+  {lesson: 4, status: true},
+  {lesson: 5, status: false},
+  {lesson: 6, status: true},
+  {lesson: 7, status: true},
+  {lesson: 8, status: true},
+  {lesson: 9, status: true},
+  {lesson: 10, status: false}
+];
 
-  let num = String(result).split('').map(function(e){return parseInt(e)}).reduce(function(a,b){return a+b});
+const falseLesson = myLesson.filter(item => item.status === false);
 
-  console.log(num);
-
-  return result;
-}
-
-console.log(getResult(4, 8));
+console.log(falseLesson);
+/*
+Доп Задание:
+Необходимо вывести в консоль массив с заданиями у которых статус false
+*/
